aa508ed664a39512671dbdb638cbe8f74bc3d4ef
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-01-11 20:25:19 +0900

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,22 @@
+import { Text, SafeAreaView, View } from 'react-native';
+import { GridComponent } from 'mogiyoon-react-native-simple-grid';
+
+const testList = [1, 2, 3, 4, 5]
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <GridComponent 
+        columnNumber={2}
+        data={testList}
+        renderItem={({item}) => (
+          <View>
+            <Text>
+              {item}
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
